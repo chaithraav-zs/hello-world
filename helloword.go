99012f26fdc1bd2ed63c868daabafb5a2654008b
chaithraav9998 <chaithra.av@zopsmart.com>
2021-02-16 15:37:03 +0530

    initial commit

---
+++ helloword.go
@@ -0,0 +1,17 @@
+package awesomeProject
+
+import (
+	"fmt"
+	"math/rand"
+)
+
+package main
+
+import (
+"fmt"
+"math/rand"
+)
+
+func main() {
+	fmt.Println("My favorite number is", rand.Intn(10))
+}
