75e302cd934443ce6d868f66ec17b4a01b8b01f7
chaithraav9998 <chaithra.av@zopsmart.com>
2021-05-19 19:16:48 +0530

    hello

--- helloword.go
+++ helloword.go
@@ -8,9 +8,15 @@ import (
 
 func main() {
 	log.Println("hey")
+
+	log.Println()
+
 	fmt.Println("My favorite number is", rand.Intn(100))
 }
 
 func hello() {
 	fmt.Println("print hello")
+}
+func hey() {
+	fmt.Println("heyyy")
 }
