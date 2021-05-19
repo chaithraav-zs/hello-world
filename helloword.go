b08865295cd25e167feea19e9a1ac9e9dc71754e
chaithraav9998 <chaithra.av@zopsmart.com>
2021-05-19 19:23:34 +0530

    nice to meet

--- helloword.go
+++ helloword.go
@@ -2,10 +2,19 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"math/rand"
 )
 
 func main() {
 	fmt.Println("hello-world")
 	fmt.Println("My favorite number is", rand.Intn(100))
+}
+func math(){
+	log.Println("nice to meet")
+	log.Println("nice to meet")
+	log.Println("nice to meet")
+	log.Println("nice to meet")
+	log.Println("nice to meet")
+	log.Println("nice to meet")
 }
