e48cc8bd40841abf56d7deb1603108bd079e9cb7
chaithraav9998 <chaithra.av@zopsmart.com>
2021-05-19 19:00:38 +0530

    changes

--- helloword.go
+++ helloword.go
@@ -2,10 +2,15 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"math/rand"
 )
 
 func main() {
-	fmt.Println("hello-world")
+	log.Println("hey")
 	fmt.Println("My favorite number is", rand.Intn(100))
+}
+
+func hello() {
+	fmt.Println("print hello")
 }
